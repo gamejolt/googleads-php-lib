8642691dcf27610e11261653ffb017d91f6677ce
hworld <admin@lucentweb.com>
2015-04-21 13:50:15 -0500

    Fixing bug with a require once breaking randomly.
    For some reason was referencing an older folder. Trying to fix it.
    PHP Fatal error:  require_once(): Failed opening required '/home/gjolt/library/googleads/googleads-php-lib/src/Google/Api/Ads/Dfp/Lib/../v201306/LineItemService.php' (include_path='/home/gjolt/library/googleads/googleads-php-lib/src:.:/usr/share/pear:/usr/share/php') in /home/gjolt/library/googleads/googleads-php-lib/src/Google/Api/Ads/Dfp/Lib/DfpSoapClientFactory.php on line 63

--- src/Google/Api/Ads/Dfp/Lib/DfpSoapClientFactory.php
+++ src/Google/Api/Ads/Dfp/Lib/DfpSoapClientFactory.php
@@ -60,7 +60,7 @@ class DfpSoapClientFactory extends SoapClientFactory {
    */
   public function DoRequireOnce($serviceName) {
     require_once implode("/", array(dirname(__FILE__), '..',
-        $this->GetVersion(), $serviceName . '.php'));
+        'v201402', $serviceName . '.php'));
   }
 }
 
